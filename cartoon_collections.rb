66ca302529ad9c4c334cc9731d623351c620d04f
Jason  Zhou <jzhou35@buffalo.edu>
2020-10-25 21:01:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  planeteer_calls.map {|elem| elem.length > 4? true:  false}
+  planeteer_calls.any? 
 end
 
 def find_valid_calls(planeteer_calls)
